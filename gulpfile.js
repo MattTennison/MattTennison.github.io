55843762bd6d42703ec7e69ecc7e02b5f0443002
Matt Tennison <matt@matttennison.com>
2018-07-28 15:13:26 +0100

    Seperate out stylesheets into components

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,13 @@ gulp.task('browser-sync', ['sass'], function() {
 });
 
 gulp.task('sass', function () {
-    return gulp.src("scss/*.scss")
+    return gulp.src("./scss/**/*.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'))
         .pipe(bs.reload({stream: true}));
 });
 
 gulp.task('watch', ['browser-sync'], function () {
-    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./scss/**/*.scss", ['sass']);
     gulp.watch("*.html").on('change', bs.reload);
 });
